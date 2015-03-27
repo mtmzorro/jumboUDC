02602b8cc965656ab69f6845a67abe57ae031eb9
mtmzorro <mtmzorro@gmail.com>
2015-03-27 18:49:05 +0800

    add mongoDB

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var user = require('../database/db').user;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/test', function(req, res, next) {
+
+    user.find({},function(err,docs){
+        console.log(docs[0].name);
+        res.render('index', { title: docs[0].name });
+    });
+
+
 });
 
 module.exports = router;
